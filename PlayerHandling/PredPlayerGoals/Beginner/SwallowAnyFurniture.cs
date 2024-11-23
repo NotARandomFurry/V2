3882ff55d41d2a8aa4cd63ff6e53c6a4f4206461
VenomiXeD <crafterious@gmail.com>
2024-11-23 19:46:08 +0100

    add swallow furniture I goal

---
+++ PlayerHandling/PredPlayerGoals/Beginner/SwallowAnyFurniture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace V2.PlayerHandling.PredPlayerGoals.Beginner
+{
+    internal class SwallowAnyFurniture : PredPlayerGoal
+    {
+        public override string InternalName => "SwallowAnyFurniture";
+
+        public override int StatPointsFromCompletion => 3;
+
+        public override ProgressionStage Stage => ModContent.GetInstance<BeginnerStage>();
+
+        public override string Description(Player pred) => "Mods.V2.PredPlayerGoals.Beginner.SwallowAnyFurniture.Description";
+        public override string DisplayName(Player pred) => "Mods.V2.PredPlayerGoals.Beginner.SwallowAnyFurniture.Name";
+    }
+}
