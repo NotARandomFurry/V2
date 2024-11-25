7c2b2176c237fe257ab27a8087b999695d7f4199
VenomiXeD <crafterious@gmail.com>
2024-11-25 14:27:37 +0100

    work on sloshes

---
+++ Items/Vanilla/VanillaGel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace V2.Items.Vanilla
+{
+    public class VanillaGel : GlobalItem
+    {
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.Gel;
+
+        public override void SetDefaults(Item entity)
+        {
+            entity.AsFood().MaxHealth = 6;
+            entity.AsFood().Size = 0.006d;
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            tooltips.AddVorariaDynamicItemTooltip("Vanilla.Gel", new { });
+        }
+    }
+}
---
+++ Sounds/Vore/Sloshes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+
+namespace V2.Sounds.Vore
+{
+    public static class Sloshes
+    {
+        public static class Humanoid
+        {
+            public static readonly SoundStyle Standard = new SoundStyle("V2/Sounds/Vore/Sloshes/Slosh_", 1, 10, SoundType.Sound);
+        }
+    }
+}
