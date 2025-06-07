1d457ed74bebbac6ea50e4869739eeaee1ec3858
VenomiXeD <crafterious@gmail.com>
2025-06-07 19:00:59 +0200

    Cleanup

--- Items/Vanilla/Accessories/VoodooDolls/GuideVoodooDoll.cs
+++ Items/Vanilla/Accessories/VoodooDolls/GuideVoodooDoll.cs
@@ -1,58 +1,44 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria;
+﻿using Terraria;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
-using V2.Core;
 using V2.PlayerHandling;
 using V2.PlayerHandling.PredPlayerGoals.Amateur;
-using V2.PlayerHandling.PredPlayerGoals.Beginner;
 
-namespace V2.Items.Vanilla.Accessories.VoodooDolls
+namespace V2.Items.Vanilla.Accessories.VoodooDolls;
+
+public class GuideVoodooDoll : GlobalItem
 {
-	public class GuideVoodooDoll : GlobalItem
-	{
-		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.GuideVoodooDoll;
+    public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+    {
+        return entity.type == ItemID.GuideVoodooDoll;
+    }
 
-		public override void SetDefaults(Item item)
-		{
-			item.AsFood().MaxHealth = 250;
-			item.AsFood().Size = 0.25;
+    public override void SetDefaults(Item item)
+    {
+        item.AsFood().MaxHealth = 250;
+        item.AsFood().Size = 0.25;
 
-			item.AsFood().OnBreak += OnBreak_GrantVoodooDigestionGoal;
-		}
+        item.AsFood().OnBreak += OnBreak_GrantVoodooDigestionGoal;
+    }
 
-		public override void Update(Item item, ref float gravity, ref float maxFallSpeed)
-		{
-			foreach (NPC npc in Main.ActiveNPCs)
-			{
-				if (npc.type == NPCID.Guide)
-				{
-					item.AsFood().Health = npc.life;
-					break;
-				}
-			}
-		}
+    public override void Update(Item item, ref float gravity, ref float maxFallSpeed)
+    {
+        foreach (var npc in Main.ActiveNPCs)
+            if (npc.type == NPCID.Guide)
+            {
+                item.AsFood().Health = npc.life;
+                break;
+            }
+    }
 
-		public static bool OnBreak_GrantVoodooDigestionGoal(Item item, Entity pred, bool direct)
-		{
-			if (pred is Player predPlayer)
-			{
-				// Terraria.Item.CheckLavaDeath
-				if (predPlayer.AsPred().StomachTracker.ContainsLiquid(LiquidID.Lava))
-				{
-					NPC.SpawnWOF(pred.Center);
-				}
+    public static bool OnBreak_GrantVoodooDigestionGoal(Item item, Entity pred, bool direct)
+    {
+        if (pred is not Player predPlayer) return true;
+        // Terraria.Item.CheckLavaDeath
+        if (predPlayer.AsPred().StomachTracker.ContainsLiquid(LiquidID.Lava)) NPC.SpawnWOF(pred.Center);
 
-				ModContent.GetInstance<DigestWithVoodoo>().TrySetCompletion(predPlayer);
-			}
+        ModContent.GetInstance<DigestWithVoodoo>().TrySetCompletion(predPlayer);
 
-			return true;
-		}
-	}
+        return true;
+    }
 }
