c28ea19219df9282621fa20d5e089db93eaebd6b
ThomasThePencil <tailsguy24@gmail.com>
2025-12-08 10:09:58 -0500

    fat bees

---
+++ Items/Vanilla/Weapons/Ranged/FlareGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace V2.Items.Vanilla.Weapons.Ranged
+{
+	public class FlareGun : GlobalItem
+	{
+		public override bool InstancePerEntity => true;
+		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.FlareGun;
+
+		public override void SetDefaults(Item item)
+		{
+			item.damage = 20;
+			item.DamageType = DamageClass.Ranged;
+			item.shootSpeed = 10f;
+
+			item.useTime = 27;
+			item.useAnimation = 27;
+			item.autoReuse = false;
+
+			item.AsFood().MaxHealth = 900;
+			item.AsFood().Size = 0.242;
+			item.AsFood().AcidResistTier = 1;
+
+			item.AsTaggable().Gun = true;
+		}
+	}
+}
