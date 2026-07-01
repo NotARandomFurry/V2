730da70506a2fc37c781485ac88192b3622c0584
VenomiXeD <crafterious@gmail.com>
2026-07-01 03:04:18 +0200

    Add flavor text to Venomizeous' Gaze. Pick one Silt digest implementation.

--- Items/Vanilla/Placeables/PlaceableTiles/ExtractableBlocks.cs
+++ Items/Vanilla/Placeables/PlaceableTiles/ExtractableBlocks.cs
@@ -1,56 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using MonoMod.Cil;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using V2.Core.MainDetours;
+using V2.PlayerHandling;
 using V2.PlayerHandling.PredPlayerGoals.Amateur;
 using V2.PlayerHandling.PredPlayerGoals.Beginner;
 using V2.Sounds.MuffledSounds;
 using V2.Sounds.Vore;
 
-namespace V2.Items.Vanilla.Placeables.PlaceableTiles
+namespace V2.Items.Vanilla.Placeables.Tiles
 {
 	public class Silt : GlobalItem
 	{
+		public static bool ExtractableBlockWasJustDigested { get; set; }
+		private static MethodInfo Player_ExtractinatorUse => typeof(Player).GetMethod(
+			"ExtractinatorUse",
+			BindingFlags.NonPublic|BindingFlags.Instance,
+			[typeof(int),typeof(int)]
+			);
 		public override bool InstancePerEntity => true;
-		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type is ItemID.SiltBlock;
+		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => 
+			entity.type is ItemID.SiltBlock or ItemID.SlushBlock;
 
 		public override void SetDefaults(Item item)
 		{
 			item.AsFood().MaxHealth = 30;
 			item.AsFood().Size = 0.1;
-		}
-		public static bool OnBreak(Item item, Entity pred, bool direct)
-		{
-			int stack = item.stack;
-			while (stack > 0)
-			{
-				stack--;
-				V2Utils.ExtractItemInTum(item.type, pred);
-			}
-			return true;
-		}
-	}
-	public class Slush : GlobalItem
-	{
-		public override bool InstancePerEntity => true;
-		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type is ItemID.SlushBlock;
-
-		public override void SetDefaults(Item item)
-		{
-			item.AsFood().MaxHealth = 30;
-			item.AsFood().Size = 0.12;
 			item.AsFood().OnBreak += OnBreak;
 		}
 		public static bool OnBreak(Item item, Entity pred, bool direct)
 		{
-			int stack = item.stack;
-			while (stack > 0)
+			SoundEngine.PlaySound(MuffledMiscSounds.Shatter, pred.Center);
+			SoundEngine.PlaySound(StomachNoises.Muffled, pred.Center);
+
+			for (int i = 0; i < item.stack; i++)
 			{
-				stack--;
-				V2Utils.ExtractItemInTum(item.type, pred);
+				if (pred is Player playerPred)
+				{
+					int extractType = ItemID.Sets.ExtractinatorMode[item.type];
+					V2Utils.MarkLootDigested(playerPred);
+					Player_ExtractinatorUse.Invoke(playerPred, [extractType, TileID.Extractinator]);
+				}
+				else if (pred is NPC NPCPred)
+				{
+
+				}
 			}
 			return true;
 		}
@@ -58,24 +58,15 @@ namespace V2.Items.Vanilla.Placeables.PlaceableTiles
 	public class Fossil : GlobalItem
 	{
 		public override bool InstancePerEntity => true;
-		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type is ItemID.DesertFossil;
+		public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.DesertFossil;
 
 		public override void SetDefaults(Item item)
 		{
 			item.AsFood().MaxHealth = 250;
 			item.AsFood().AcidResistTier = 1;
 			item.AsFood().Size = 0.15;
-			item.AsFood().OnBreak += OnBreak;
-		}
-		public static bool OnBreak(Item item, Entity pred, bool direct)
-		{
-			int stack = item.stack;
-			while (stack > 0)
-			{
-				stack--;
-				V2Utils.ExtractItemInTum(item.type, pred);
-			}
-			return true;
+
+			item.AsFood().OnBreak += Silt.OnBreak;
 		}
 	}
 }
